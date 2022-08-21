41bcd42344dc9cab02863a817a712c17b4946a39
GStarP <1700066935@qq.com>
2022-08-21 16:33:31 +0800

    test: 补充 lazy effect 测试用例

--- packages/reactivity/__tests__/effect.spec.ts
+++ packages/reactivity/__tests__/effect.spec.ts
@@ -1,6 +1,9 @@
+import { effect } from '../src/effect';
 import { reactive } from '../src/reactive';
 
+// 测试：effect 模块
 describe('effect', () => {
+  // 测试一：未经 effect 声明的函数不被响应式
   test('only effect(fn) is reactive', () => {
     let original = { a: 1 };
     let observed = reactive(original);
@@ -13,5 +16,23 @@ describe('effect', () => {
     setB();
     observed.a = 2;
     expect(b).toBe(0);
+  });
+  // 测试二：设置 options.lazy 将会延迟响应式声明
+  test('lazy effect', () => {
+    let original = { a: 1 };
+    let observed = reactive(original);
+    expect(observed).not.toBe(original);
+    let b = 0;
+    function setB() {
+      b = observed.a - 1;
+    }
+    const runner = effect(setB, { lazy: true });
+    observed.a = 2;
+    // 未执行 runner，setB 未被响应式
+    expect(b).toBe(0);
+    // 已执行 runner，setB 已被响应式
+    runner();
+    observed.a = 10;
+    expect(b).toBe(9);
   });
 });
